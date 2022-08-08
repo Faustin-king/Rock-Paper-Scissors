b179b1f96d1792d974b56fb732cb806856dbe01f
Mystery <f.nduwumwamiwabami@gmail.com>
2022-08-08 17:09:12 +0200

    Update game function

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const choices = ["rock", "paper", "scissors"];
 let winners = [];
 
 function game() {
-  // for (let i = 1; i <= 5; i++) {
-  //   playRound(i);
-  // }
+  for (let i = 1; i <= 5; i++) {
+    playRound(i);
+  }
   document.querySelector("button").textContent = "Play new game";
   logWins();
 }
